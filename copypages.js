636b0355d0a78aff081f7b677652b8581e337708
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2026-08-06 09:18:28 -0300

    metatags

--- copypages.js
+++ copypages.js
@@ -8,5 +8,7 @@ import cpy from 'cpy';
     await cpy('nosotros.html', 'dist');
     await cpy('contacto.html', 'dist');
     await cpy('.htaccess', 'dist');
+    await cpy('sitemap.xml', 'dist');
+    await cpy('robots.txt', 'dist');
 
 })();
